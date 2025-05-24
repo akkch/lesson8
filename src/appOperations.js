2c822547368e571b720b38e20be3e55b16774f83
akkch <42873403+akkch@users.noreply.github.com>
2025-05-24 14:45:26 +0300

    added WF to publish the PKG and fixed add func

--- src/appOperations.js
+++ src/appOperations.js
@@ -1,5 +1,16 @@
 sampleSecret = "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";
 
+function add(num1, num2) {
+  const intNum1 = parseInt(num1,10);
+  const intNum2 = parseInt(num2,10);
+
+  return intNum1 + intNum2;
+}
+
+function substract(num1, num2) {
+  return num1 + num2;
+}
+
 function multiply(num1, num2) {
   return num1 * num2;
 }
@@ -11,19 +22,9 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function divideV2(num1, num2) {
-  if (num2 === 0) {
-    throw new Error("Cannot divide by zero");
-  } 
-    return num1 / num2;
-}
-
-function add(num1, num2) {
-  return num1 + num2;
-}
 module.exports = {
+  add,
+  substract,
   multiply,
   divide,
-  divideV2,
-  add,
 };
