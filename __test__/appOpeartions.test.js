0a432ca1068cb400fcd686f9e4c2b2f7f8a2825f
akkch <42873403+akkch@users.noreply.github.com>
2025-05-24 15:08:41 +0300

    Tests repaired for appOperations.js

--- __test__/appOpeartions.test.js
+++ __test__/appOpeartions.test.js
@@ -1,27 +1,23 @@
     const appOperations = require('../src/appOperations');
-
-    test("0 * 5 = 0", () => {
-        expect(appOperations.multiply(0, 5)).toBe(0);
-    });
     
     test("5 + 5 = 10", () => {
         expect(appOperations.add(5, 5)).toBe(10);
     });
     
+    test("10 - 5 = 5", () => {
+        expect(appOperations.substract(10, 5)).toBe(5);
+    });
+    
+    test("0 * 5 = 0", () => {
+        expect(appOperations.multiply(0, 5)).toBe(0);
+    });
+
     test("6 / 3 = 2", () => {
         expect(appOperations.divide(6, 3)).toBe(2);
     });
 
-    test("6 / 3 = 2", () => {
-        expect(appOperations.divideV2(6, 3)).toBe(2);
-    });
-
      test("6 / 0 = error", () => {
         expect(() => appOperations.divide(6, 0)).toThrow("Cannot divide by zero");
-        });
-
-        test("6 / 0 = error (v2)", () => {
-        expect(() => appOperations.divideV2(6, 0)).toThrow("Cannot divide by zero");
         });
     
 
